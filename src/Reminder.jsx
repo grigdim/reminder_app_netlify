0c019d7798aabc7396170035c3a53c5be62c082f
grigdim <84345122+grigdim@users.noreply.github.com>
2022-01-17 07:54:22 +0200

    Update Reminder.jsx

--- src/Reminder.jsx
+++ src/Reminder.jsx
@@ -1,30 +1,45 @@
-import React from 'react';
-import { format } from 'date-fns';
+import React from "react";
+import { format } from "date-fns";
 const Reminder = ({ reminder, handleDelete, handleCheck }) => {
-	return (
-		<li className="reminder">
-			<div className="reminderInfo">
-				<label style={reminder.checked ? { textDecoration: 'line-through' } : null}>
-					<h4>{`Reminder Title: ${reminder.title}`}</h4>
-					<p className="date">{`This event is on: ${format(new Date(reminder.date), 'MMMM dd, yyyy pp')}`}</p>
-				{reminder.frequency !== 'do not repeat' && (
-					<p>
-							This reminder has a {reminder.frequency} frequency and has been triggered{' '}
-							{reminder.triggered}  {reminder.triggered === 1 ? 'time' : 'times'}
-					</p>
-				)}
-				</label>
-			</div>
-			<div className="buttons">
-				<button className="checked" onClick={() => handleCheck(reminder.id)} checked={reminder.checked}>
-					Done
-				</button>
-				<button className="delete" onClick={() => handleDelete(reminder.id)}>
-					Delete
-				</button>
-			</div>
-		</li>
-	);
+  return (
+    <li className="reminder">
+      <div className="reminderInfo">
+        <label
+          style={reminder.checked ? { textDecoration: "line-through" } : null}
+        >
+          <h4>{`Reminder Title: ${reminder.title}`}</h4>
+          <p className="date">{`This event is on: ${format(
+            new Date(reminder.date),
+            "MMMM dd, yyyy pp"
+          )}`}</p>
+          {reminder.frequency !== "do not repeat" && (
+            <p>
+              This reminder has a {reminder.frequency} frequency and has been
+              triggered {reminder.triggered}{" "}
+              {reminder.triggered === 1 ? "time" : "times"}
+            </p>
+          )}
+        </label>
+      </div>
+      <div className="buttons">
+        <button
+          className="checked"
+          onClick={() => handleCheck(reminder.id)}
+          checked={reminder.checked}
+          style={
+            reminder.checked
+              ? { backgroundColor: "rgba(0, 89, 255, 0.75)" }
+              : { backgroundColor: "rgba(0, 200, 0, 0.75)" }
+          }
+        >
+          {reminder.checked ? "Uncheck" : "Check"}
+        </button>
+        <button className="delete" onClick={() => handleDelete(reminder.id)}>
+          Delete
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default Reminder;
